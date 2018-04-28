01cef8427c13ab4bad5cf9063b92f960ff321fa9
liukhang <liukhang123@gmail.com>
2018-04-28 11:13:31 +0700

    up

---
+++ skin/frontend/rwd/intimidea/jqtransformplugin/intimidea.js
@@ -0,0 +1,7 @@
+jQuery(function(){
+    jQuery('.checkbox-left').jqTransform({imgPath:'img/'});
+    jQuery('.checkbox-right').jqTransform({imgPath:'img/'});
+    jQuery('.firecheckout-index-index .newsletter.control').jqTransform({imgPath:'img/'});
+    jQuery('.firecheckout-index-index .control').jqTransform({imgPath:'img/'});
+    jQuery('.sp-methods dt').jqTransform({imgPath:'img/'});
+});
